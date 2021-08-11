57a51ad9c38ad0aa1f78111534eec3650f2e3f07
Gaurang1810 <gaurangparmar1810@gmail.com>
2021-08-11 16:01:07 +0530

    First cpp file is added

---
+++ DigitSum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        int num,x=0;
+        cin >> num;
+       cout << (num+1)/10 << endl;
+        
+    }
+    return 0;
+}
